85f5610c7781a9e5bed8d37cf9684a41038bb976
Andrew <andrejpopkov565@gmail.com>
2025-05-12 16:59:23 +0300

    added Home Page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,24 @@
+import { Link } from 'react-router-dom';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={css.homePageContainer}>
+      <section className={css.homePageContainer__homeSection}>
+        <div>
+          <h1 className={css.homeSection__homeHeader}>
+            Find your perfect rental car
+          </h1>
+          <p className={css.homeSection__homeTitle}>
+            Reliable and budget-friendly rentals for any journey
+          </p>
+        </div>
+        <Link className={`blue-btn`} to="/catalog">
+          View Catalog
+        </Link>
+      </section>
+    </div>
+  );
 };
 
 export default HomePage;
